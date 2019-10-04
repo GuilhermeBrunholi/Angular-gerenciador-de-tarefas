f65c4fd97f23954e4f812877a9da50ec515bcb8e
Guilherme Brunholi <gui_brunholi@hotmail.com>
2019-10-04 11:28:07 -0300

    Criado HTML cadastro

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TarefaService, Tarefa } from '../shared';
 
 @Component({
   selector: 'app-listar-tarefa',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarTarefaComponent implements OnInit {
 
-  constructor() { }
+  tarefas: Tarefa[];
+
+  constructor(private tarefaService: TarefaService) { }
 
   ngOnInit() {
+    this.tarefas = this.listarTodos();
+    this.tarefas = [
+      new Tarefa(1, "Tarefa 1", false),
+      new Tarefa(2, "Tarefa 2", true)
+    ];
   }
+
+  listarTodos(): Tarefa[] {
+    return this.tarefaService.listarTodos();
+  }
+
+  // remover($event: any, tarefa: Tarefa): void {
+  //   $event.preventDefault();
+  //   if(confirm('Deseja remover a tarefa"' + tarefa.nome + '"?'))
+  //   this.tarefaService.remover(tarefa.id);
+  //   this.tarefas = this.tarefaService.listarTodos();
+  // }
+
 
 }
